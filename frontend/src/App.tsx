3ce55763f90193e092f0e25999477bbe8c7eae01
Davi Silva <davisilvaphoto@gmail.com>
2022-03-25 15:28:04 -0300

    front end with graphql

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+import { NewUserForm } from "./components/NewUserForm";
+
+export const GET_USER = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+type User = {
+  id: string;
+  name: string;
+};
+
+function App() {
+  const { data, loading } = useQuery<{ users: User[] }>(GET_USER);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <NewUserForm />
+
+      <ul>
+        {data?.users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
